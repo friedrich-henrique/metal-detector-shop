94a8e8cbf7cfa86c7840ba1b100484efd949fd83
Henrique <friedrich@mailbox.unideb.hu>
2023-04-18 17:36:34 +0200

    added detector raquete

--- src/pages/shop.js
+++ src/pages/shop.js
@@ -2,6 +2,8 @@ import Loading from '@/components/Loading';
 import axios from '@/lib/axios'
 import Image from 'next/image';
 import { useState, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const CartButton = ({ openCart, count }) => {
     return <>
@@ -21,7 +23,7 @@ const CartButton = ({ openCart, count }) => {
     </>
 }
 
-const ShippingDetails = ({ changeState }) => {
+const ShippingDetails = ({ changeCartState, cart, resetParentCart }) => {
     const csrf = () => axios.get('/sanctum/csrf-cookie')
     const [email, setEmail] = useState('');
     const [endereco, setEndereco] = useState('');
@@ -31,10 +33,34 @@ const ShippingDetails = ({ changeState }) => {
     const [telefone, setTelefone] = useState('');
     const [errors, setErrors] = useState({});
 
-
     const setParentState = (state) => {
-        changeState(state)
+        changeCartState(state)
     }
+
+    const notifySuccess = () =>
+        toast.success('Pedido realizado!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+
+        const notifyError = () =>
+        toast.error('Tivemos um erro ao processar seu pedido!', {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+        });
+
     const handleSubmit = async (e) => {
         await csrf()
         setParentState('loading')
@@ -47,10 +73,15 @@ const ShippingDetails = ({ changeState }) => {
             cidade: cidade,
             estado: estado,
             cep: cep,
-            telefone: telefone
+            telefone: telefone,
+            products: cart
         }).then(
-            setParentState('success')
+            setParentState('success'),
+            resetParentCart(),
+            notifySuccess()
         ).catch(err => {
+            notifyError()
+            setParentState('error'),
             console.log(errors)
             setErrors(err.response)
         })
@@ -151,11 +182,10 @@ const ShippingDetails = ({ changeState }) => {
     </>
 }
 
-const Cart = ({ cart, removeProductFromCart, closeCart }) => {
-    const [state, setState] = useState('default')
+const Cart = ({ cart, removeProductFromCart, closeCart, resetParentCart }) => {
+    const [state, setState] = useState(null)
 
-    const changeState = (state) => {
-        console.log(state)
+    const changeCartState = (state) => {
         setState(state)
     }
     return <>
@@ -165,82 +195,69 @@ const Cart = ({ cart, removeProductFromCart, closeCart }) => {
                 <div className="grid place-content-center h-80">
                     <Loading></Loading>
                 </div>
-
-                : state === 'success' ?
-                    <div class="p-8 space-y-4">
-                        <div class="flex w-96">
-                            <div class="bg-green-600 py-4 px-6 rounded-lg flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="text-white fill-current" viewBox="0 0 16 16" width="20" height="20"><path fill-rule="evenodd" d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"></path></svg>
-                            </div>
-                            <div class="px-4 py-6 flex justify-between items-center w-full">
-                                <div>Pedido enviado</div>
-                            </div>
-                        </div>
-                    </div>
-                    :
-                    <div>
-                        <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
-                            <div class="flex items-start justify-between">
-                                <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Carrinho</h2>
-                                <div class="ml-3 flex h-7 items-center">
-                                    <button type="button" class="-m-2 p-2 text-gray-400 hover:text-gray-500" onClick={() => closeCart()}>
-                                        <span class="sr-only">Fechar carrinho</span>
-                                        <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                                        </svg>
-                                    </button>
-                                </div>
-                            </div>
-
-                            <div class="mt-8">
-                                <div class="flow-root">
-                                    <ul role="list" class="-my-6 divide-y divide-gray-200">
-                                        {cart.length == 0 &&
-                                            <div class="flex justify-center py-6">
-                                                Seu carrinho ainda está vazio.
-                                            </div>
-                                        }
-                                        {
-                                            cart.map((item) => {
-                                                return <li class="flex py-6">
-                                                    <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
-                                                        <Image
-                                                            src={"/" + item.image}
-                                                            class="h-full w-full object-cover object-center"
-                                                            alt={item.nome}
-                                                            width={100}
-                                                            height={100} />
-                                                    </div>
-
-                                                    <div class="ml-4 flex flex-1 flex-col">
-                                                        <div>
-                                                            <div class="flex justify-between text-base font-medium text-gray-900">
-                                                                <h3>
-                                                                    <a href="#">{item.nome}</a>
-                                                                </h3>
-                                                            </div>
-                                                            <p class="mt-1 text-sm text-gray-500">{item.descrição}</p>
-                                                        </div>
-                                                        <div class="flex flex-1 items-end justify-between text-sm">
-                                                            <p class="text-gray-500">Qte {item.quantity}</p>
-
-                                                            <div class="flex">
-                                                                <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => removeProductFromCart(item)}>Remover</button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </li>
-                                            })
-                                        }
-                                    </ul>
-                                </div>
+                :
+                <div>
+                    <div class="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+                        <div class="flex items-start justify-between">
+                            <h2 class="text-lg font-medium text-gray-900" id="slide-over-title">Carrinho</h2>
+                            <div class="ml-3 flex h-7 items-center">
+                                <button type="button" class="-m-2 p-2 text-gray-400 hover:text-gray-500" onClick={() => closeCart()}>
+                                    <span class="sr-only">Fechar carrinho</span>
+                                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
                             </div>
                         </div>
-                        {
-                            cart.length !== 0 &&
-                            <ShippingDetails changeState={changeState} />
-                        }
+                        <div class="mt-8">
+                            <div class="flow-root">
+                                <ul role="list" class="-my-6 divide-y divide-gray-200">
+                                    {cart.length == 0 &&
+                                        <div class="flex justify-center py-6">
+                                            Seu carrinho ainda está vazio.
+                                        </div>
+                                    }
+                                    {
+                                        cart.map((item) => {
+                                            return <li class="flex py-6">
+                                                <div class="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+                                                    <Image
+                                                        src={"/" + item.image}
+                                                        class="h-full w-full object-cover object-center"
+                                                        alt={item.nome}
+                                                        width={100}
+                                                        height={100} />
+                                                </div>
+
+                                                <div class="ml-4 flex flex-1 flex-col">
+                                                    <div>
+                                                        <div class="flex justify-between text-base font-medium text-gray-900">
+                                                            <h3>
+                                                                <a href="#">{item.nome}</a>
+                                                            </h3>
+                                                        </div>
+                                                        <p class="mt-1 text-sm text-gray-500">{item.descrição}</p>
+                                                    </div>
+                                                    <div class="flex flex-1 items-end justify-between text-sm">
+                                                        <p class="text-gray-500">Qte {item.quantity}</p>
+
+                                                        <div class="flex">
+                                                            <button type="button" class="font-medium text-indigo-600 hover:text-indigo-500" onClick={() => removeProductFromCart(item)}>Remover</button>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </li>
+                                        })
+                                    }
+                                </ul>
+                            </div>
+                            {
+                                cart.length !== 0 &&
+                                <ShippingDetails changeCartState={changeCartState} cart={cart} resetParentCart={resetParentCart} />
+                            }
+                        </div>
                     </div>
+                </div>
         }
     </>
 }
@@ -256,7 +273,7 @@ const Product = ({ product, addToCart }) => {
                         src={`/${product.image}`} // Route of the image file
                         width={384} // Desired size with correct aspect ratio
                         height={150} // Desired size with correct aspect ratio
-                        alt="{product.nome}"
+                        alt={`Imagem do ${product.nome}`}
                     />
                 </a>
                 <div class="p-6">
@@ -289,8 +306,9 @@ const Shop = () => {
     const [errors, setErrors] = useState([])
     const [cart, setCart] = useState([])
     const [subtotal, setSubtotal] = useState(0)
-    const [showCart, setShowCart] = useState(false)
+    const [cartState, setCartState] = useState('closed')
     const [count, setCount] = useState(0)
+        
     useEffect(() => {
         fetchProducts()
     }, [])
@@ -310,7 +328,6 @@ const Shop = () => {
         } else {
             setSubtotal(formatPrice(0))
             setCount(0)
-            setShowCart(false)
         }
     }, [cart])
 
@@ -333,6 +350,11 @@ const Shop = () => {
         })
     }
 
+    function resetCart() {
+        setCart([])
+        setCartState('closed')
+    }
+
     function formatPrice(price) {
         return price.toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })
     }
@@ -340,6 +362,7 @@ const Shop = () => {
     function addToCart(product) {
         product.quantity = 1
         const existingItemIndex = cart.findIndex((item) => item.id === product.id);
+        setCartState('open')
         if (existingItemIndex !== -1) {
             const existingItem = cart[existingItemIndex];
             const updatedItem = {
@@ -352,7 +375,7 @@ const Shop = () => {
         } else {
             setCart([...cart, product])
         }
-        setShowCart(true)
+        setCartState('open')
     }
 
     function removeProductFromCart(product) {
@@ -360,14 +383,13 @@ const Shop = () => {
     }
 
     function closeCart() {
-        setShowCart(false)
+        setCartState('closed')
     }
 
     function openCart() {
-        setShowCart(true)
+        setCartState('open')
     }
     return <>
-
         {
             state === 'loading' &&
             <div className="grid place-content-center h-80">
@@ -387,15 +409,28 @@ const Shop = () => {
                     </div>
                 }
                 {
-                    showCart &&
+                    cartState == 'open' &&
                     <div class="flex flex-col overflow-y-scroll h-full bg-white shadow-xl lg:w-1/4 right-0 transition">
-                        <Cart cart={cart} subtotal={subtotal} removeProductFromCart={removeProductFromCart} closeCart={closeCart} />
+                        <Cart cart={cart} subtotal={subtotal} removeProductFromCart={removeProductFromCart} closeCart={closeCart} resetParentCart={resetCart} />
                     </div>
                 }
                 {
-                    !showCart &&
+                    cartState === 'closed' &&
                     <CartButton openCart={openCart} count={count} />
                 }
+
+                <ToastContainer
+                    position="bottom-right"
+                    autoClose={5000}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                />
             </div>
         }
     </>
