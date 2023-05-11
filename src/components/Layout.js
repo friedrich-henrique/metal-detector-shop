1c486b8ec6328e53d7b49c6cf7ca6203ccd62f99
Henrique <friedrich@mailbox.unideb.hu>
2023-05-11 15:24:55 +0200

    added google analytcs script to page

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -7,8 +7,20 @@ export default function Layout({ children }) {
     return (
         <>
             <Head>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-PQP5GGCBJ6"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-PQP5GGCBJ6');
+              `,
+                    }}
+                />
                 <title>SAVEL</title>
             </Head>
+
             <main className="min-h-screen relative bg-flex flex-col">
                 <Navbar></Navbar>
                 <div className="lg:pb-28">
